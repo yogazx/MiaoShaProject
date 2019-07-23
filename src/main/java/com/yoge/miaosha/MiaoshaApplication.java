e9fff02d2cc6300f5079b56356fc79782478f360
geyoujia <geyoujia@u51.com>
2019-07-24 00:27:00 +0800

    修复包名错误

--- src/main/java/com/yoge/miaosha/MiaoshaApplication.java
+++ src/main/java/com/yoge/miaosha/MiaoshaApplication.java
@@ -1,9 +1,11 @@
 package com.yoge.miaosha;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yoge.miaosha.dao")
 public class MiaoshaApplication {
 
     public static void main(String[] args) {
